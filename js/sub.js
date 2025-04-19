4fe29d45e775e4ee9da0fe7c15fe463979d92280
Ivy <137126457+sungjinyeong@users.noreply.github.com>
2025-04-19 12:42:54 +0900

    save

--- js/sub.js
+++ js/sub.js
@@ -35,3 +35,27 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+
+
+
+const $counters = $(".scroll_on");
+const exposurePercentage = 100;
+const loop = true;
+
+$(window).on('scroll', function () {
+  $counters.each(function () {
+    const $el = $(this);
+    const rect = $el[0].getBoundingClientRect();
+    const winHeight = window.innerHeight;
+    const contentHeight = rect.bottom - rect.top;
+
+    if (rect.top <= winHeight - (contentHeight * exposurePercentage / 100) && rect.bottom >= (contentHeight * exposurePercentage / 100)) {
+        $el.addClass('active');
+    }
+
+    if (loop && (rect.bottom <= 0 || rect.top >= window.innerHeight)) {
+        $el.removeClass('active');
+    }
+  });
+}).scroll();
